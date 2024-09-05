e3306002e8b6a336ce7c22c3c86d570fd9f83929
antoo69 <antobarry69@gmail.com>
2024-09-05 18:14:49 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,12 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
+# Memuat file .env
 load_dotenv()
 
-# Telegram API credentials
-API_ID = os.getenv("API_ID")
+API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID"))
-
-# Files for storing blacklist and active groups
 BLACKLIST_FILE = os.getenv("BLACKLIST_FILE", "blacklist.txt")
-ACTIVE_GROUPS_FILE = os.getenv("ACTIVE_GROUPS_FILE", "active_groups.json")
-
-# Other configurations
-DEBUG = bool(os.getenv("DEBUG", "False"))
+ACTIVE_GROUPS_FILE = os.getenv("ACTIVE_GROUPS_FILE", "active_groups.txt")

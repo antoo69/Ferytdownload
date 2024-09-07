499c3c8113c1f52e2cfaf1dbab06c16d04e48b31
antoo69 <antobarry69@gmail.com>
2024-09-08 01:47:58 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
-from dotenv import load_dotenv
 import os
 
-# Load environment variables from a .env file
-load_dotenv()
-
-# Telegram Bot API credentials
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
+class Config:
+    API_ID = os.getenv("API_ID")
+    API_HASH = os.getenv("API_HASH")
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    MONGO_URI = os.getenv("MONGO_URI")
+    OWNER_ID = int(os.getenv("OWNER_ID"))  # ID Telegram Owner
+    LOGO_URL = os.getenv("LOGO_URL")  # URL untuk gambar logo
+    SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL")  # URL support channel
+    SUPPORT_GROUP = os.getenv("SUPPORT_GROUP")  # URL support group
+    OWNER_USERNAME = os.getenv("OWNER_USERNAME")  # Username Telegram owner

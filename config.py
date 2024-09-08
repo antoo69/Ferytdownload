c2854bfecf14908d7ced958e47973964e7f9f376
antoo69 <antobarry69@gmail.com>
2024-09-08 16:35:26 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 BOT_TOKEN = 'YOUR_BOT_TOKEN'
 
 # Daftar ID admin (gantikan dengan ID yang valid)
-ADMIN_IDS = [123456789, 987654321]  # Contoh ID admin
+ADMIN_ID = [123456789, 987654321]  # Contoh ID admin
 
 # Logo bot (URL atau path file logo)
 LOGO_PATH = 'path_to_logo_file.png'  # Ganti dengan path file logo Anda
